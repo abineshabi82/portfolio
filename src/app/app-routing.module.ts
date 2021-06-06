9381820704db2a2ebea2584502569e2e533ff953
abinesh <abineshabi82@gmail.com>
2021-06-07 00:47:52 +0530

    router inited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpComponent } from './components/exp/exp.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { WorkComponent } from './components/work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/intro', pathMatch: 'full' },
+  { path: 'intro', component: IntroComponent },
+  { path: 'exp', component: ExpComponent },
+  { path: 'skill', component: SkillComponent },
+  { path: 'work', component: WorkComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { ExpComponent } from './components/exp/exp.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { WorkComponent } from './components/work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    ExpComponent,
+    SkillComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
