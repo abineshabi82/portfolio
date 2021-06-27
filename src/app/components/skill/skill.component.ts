34044455f2ddb536c0befbaf7656e853f4721f02
abinesh <abineshabi82@gmail.com>
2021-06-27 23:09:33 +0530

    skill responsive inited

--- src/app/components/skill/skill.component.ts
+++ src/app/components/skill/skill.component.ts
@@ -1,20 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ScriptprogramComponent } from './skillsets/scriptprogram/scriptprogram.component';
 import { FrameworklibraryComponent } from './skillsets/frameworklibrary/frameworklibrary.component';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-skill',
   templateUrl: './skill.component.html',
   styleUrls: ['./skill.component.scss']
 })
-export class SkillComponent implements OnInit {
-
+export class SkillComponent implements OnInit,AfterViewInit {
+  private skillContent:any=null;
+  private skillImg:any=null;
   public chart: any = "default";
-  constructor() {
+  constructor(public router:Router) {
   }
 
   ngOnInit(): void {
+    this.skillContent=document.querySelector('.skill-container .skill-content');
+    this.skillImg=document.querySelector('.skill-container .skill-image');
+  }
 
+  ngAfterViewInit(){
+    //FOR SLIDELEFT AND DISAPPEAR
+    let flag=0;
+    this.router.events.subscribe(x=>{
+      if(x instanceof NavigationStart && flag==0){
+        flag=1;
+        let route=x.url.slice(1);
+        let body:any=document.querySelector('body');
+        if(route=="exp"){
+          body.style.background="linear-gradient(180deg, #f7e96d 0%, rgba(255, 243, 144, 1) 65%, #f7e96d 100%)";
+        }else if(route=="intro"){
+          body.style.background="linear-gradient(180deg, #7bb0c8 0%, #ace5ff 70%, #7bb0c8 99%)";
+        }else if(route=="work"){
+          body.style.background="linear-gradient(180deg, #fd8f8f 0%, #ffb99a 70%, #fd8f8f 99%)";
+        }
+        this.router.navigate(['skill']);
+        let containermain:any=document.querySelector('.skill-container');
+        let nav:any=document.querySelector('.navbar');
+        this.skillContent.style.animation="final-disappear .5s ease-in forwards";
+        setTimeout(()=>{
+          this.skillImg.style.animation="final-disappear .5s ease-in forwards";
+          nav.style.zIndex=1;
+        },300);
+        setTimeout(()=>{
+          containermain.classList.add('slide-left');
+        },800);
+        setTimeout(()=>{
+            this.router.navigate([route]);
+            nav.style.zIndex=0;
+        },1800);
+      }
+    });
   }
 
   changeChart(value: any) {
